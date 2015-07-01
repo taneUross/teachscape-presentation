798d81f0db164e9568f368a4c6a5d06d392f7cc3
jimgriesemer <jim.griesemer@teachscape.com>
2015-06-30 17:03:28 -0700

    Updates_06-30-2015_5pm
    Tex boxes, text area,

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1998,4 +1998,19 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery"
     })
   })
 
+  // TEXTAREA COUNTER
+  // =================
+
+  $(document).ready(function() {
+    var text_max = 200;
+    $('#textarea_feedback').html(text_max + ' characters remaining');
+
+    $('#textarea').keyup(function() {
+        var text_length = $('#textarea').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#textarea_feedback').html(text_remaining + ' characters remaining');
+    });
+});
+
 }(window.jQuery);
