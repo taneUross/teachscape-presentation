9cdf1dd91649ab7aee28668d6e472148bcdd5946
jimgriesemer <jim.griesemer@teachscape.com>
2015-07-08 15:21:41 -0700

    Updates-commit-only_07-08-2015_3pm

--- assets/js/jquery-1.10.2.js
+++ assets/js/jquery-1.10.2.js
@@ -9799,4 +9799,24 @@ $(document).ready(function() {
     });
 });
 
+$(function() {
+  var b = $("#button");
+  var w = $("#wrapper");
+  var l = $("#list");
+  
+  w.height(l.outerHeight(true));
+
+  b.click(function() {
+  
+    if(w.hasClass('open')) {
+      w.removeClass('open');
+      w.height(0);
+    } else {
+      w.addClass('open');
+      w.height(l.outerHeight(true));
+    }
+  
+  });
+});
+
 })( window );
